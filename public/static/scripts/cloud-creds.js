ed37b2904c374ece9fcb2a4b07ea2af7e8d07a3c
gvensan <4477169+gvensan@users.noreply.github.com>
2023-04-11 17:47:04 +0530

    remove cloud creds from the scripts

--- public/static/scripts/cloud-creds.js
+++ public/static/scripts/cloud-creds.js
@@ -1,4 +1,4 @@
-const MQTT_HOST = 'mr-connection-jbh9h93b8id.messaging.solace.cloud';
+const MQTT_HOST = '';
 const MQTT_PORT = 8443;
-const MQTT_USER_NAME = 'solace-cloud-client';
-const MQTT_PASSWORD = 'ucjt83ft2a49r3uqq5oolepqg0';
+const MQTT_USER_NAME = '';
+const MQTT_PASSWORD = '';
